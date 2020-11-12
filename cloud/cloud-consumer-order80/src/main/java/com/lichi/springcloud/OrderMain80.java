fb3f8d8e81facb4f6e8784691bf6488c63a74f02
lichilichi <2291828485@qq.com>
2020-11-12 14:56:42 +0800

    Eureka集群模式部署 支付服务提供端做负载均衡

--- cloud/cloud-consumer-order80/src/main/java/com/lichi/springcloud/OrderMain80.java
+++ cloud/cloud-consumer-order80/src/main/java/com/lichi/springcloud/OrderMain80.java
@@ -2,8 +2,10 @@ package com.lichi.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
